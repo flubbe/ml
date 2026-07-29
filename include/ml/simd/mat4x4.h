ac1197b377da9671a137faa19646fa2bad7d96e2
Felix Lubbe <fe.lubbe@gmail.com>
2026-07-29 22:45:07 +0200

    Fix 4x4 inverse.

--- include/ml/simd/mat4x4.h
+++ include/ml/simd/mat4x4.h
@@ -334,12 +334,11 @@ private:
         det = _mm_add_ps(_mm_shuffle_ps(det, det, 0x4E), det);
         det = _mm_add_ss(_mm_shuffle_ps(det, det, 0xB1), det);
 
-        // Negated to account for the initial matrix transpose and cofactor signs
-        float d = -_mm_cvtss_f32(det);
-
+        float d = _mm_cvtss_f32(det);
         if(determinant != nullptr)
         {
-            *determinant = d;
+            // Negated to account for the initial matrix transpose and cofactor signs
+            *determinant = -d;
         }
 
         const bool invertible = std::abs(d) >= epsilon;
@@ -350,7 +349,6 @@ private:
 
         if(out != nullptr)
         {
-            // Re-order permuted rows and transpose back to final matrix layout
             row0 = cofactor0;
             row1 = cofactor3;
             row2 = cofactor2;
